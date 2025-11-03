fd0984e181bb8b74bc76b6bc90db5ae86dbcc23e
Matteo Del Moro - matteo.delmoro@studio.unibo.it <matteo.delmoro@studio.unibo.it>
2025-11-03 12:04:51 +0100

    Started the implementation of IteratorWithPolicy class

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -1,5 +1,27 @@
 package it.unibo.inner.impl;
 
-public class IterableWithPolicyImpl {
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+import it.unibo.inner.api.IterableWithPolicy;
+
+public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
+    private ArrayList<T> elems = new ArrayList<>();
+
+    
+    public IterableWithPolicyImpl(final ArrayList<T> elems) {
+
+    }
+
+    private class IteratorWithPolicy implements Iterator<T> {
+        public T next() {
+            final T elem = IterableWithPolicyImpl.this.elems.getFirst();
+            IterableWithPolicyImpl.this.elems.
+        }
+
+        public boolean hasNext() {
+            return (IterableWithPolicyImpl.this.elems != null);
+        }
+    }
 }
