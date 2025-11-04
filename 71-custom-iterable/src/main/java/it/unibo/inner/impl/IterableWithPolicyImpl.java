c145cd82ceea98b76dcd17d0db4ebde5f164e8ac
Matteo Del Moro <matteo.delmoro@studio.unibo.it>
2025-11-04 09:54:20 +0100

    Solved part 2 71-costum-iterable

--- 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
+++ 71-custom-iterable/src/main/java/it/unibo/inner/impl/IterableWithPolicyImpl.java
@@ -2,20 +2,34 @@ package it.unibo.inner.impl;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-
+import java.util.NoSuchElementException;
 
 import it.unibo.inner.api.IterableWithPolicy;
 import it.unibo.inner.api.Predicate;
 
 public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
     private ArrayList<T> elems;
+    private Predicate<T> predicate;
 
-    
     public IterableWithPolicyImpl(final T[] elementsToAdd) {
+        this(elementsToAdd, new Predicate<T>() {
+
+            @Override
+            public boolean test(T elem) {
+                return true;
+            }
+
+        });
+    }
+
+    public IterableWithPolicyImpl(final T[] elementsToAdd, final Predicate<T> predicate) {
         this.elems = new ArrayList<>();
         for (final T elem : elementsToAdd) {
             this.elems.add(elem);
         }
+
+        this.predicate = predicate;
+
     }
 
     @Override
@@ -25,7 +39,7 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
 
     @Override
     public void setIterationPolicy(Predicate<T> filter) {
-
+        this.predicate = filter;
     }
 
     public String toString() {
@@ -37,11 +51,17 @@ public class IterableWithPolicyImpl<T> implements IterableWithPolicy<T> {
 
         @Override
         public boolean hasNext() {
+            while (index < elems.size() && !predicate.test(elems.get(index))) {
+                index++;
+            }
             return index < elems.size();
         }
 
         @Override
         public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
             return elems.get(index++);
         }
 
