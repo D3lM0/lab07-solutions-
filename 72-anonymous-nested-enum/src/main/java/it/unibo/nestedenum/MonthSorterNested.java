b704ad40ff3a777b38cf8e9f6819a612ebc018c8
Matteo Del Moro <matteo.delmoro@studio.unibo.it>
2025-11-04 12:12:23 +0100

    Implemented Enum Month (72-anonymous-nested-enum)

--- 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
+++ 72-anonymous-nested-enum/src/main/java/it/unibo/nestedenum/MonthSorterNested.java
@@ -8,6 +8,38 @@ import java.util.Objects;
  * Implementation of {@link MonthSorter}.
  */
 public final class MonthSorterNested implements MonthSorter {
+    private enum Month {
+        JANUARY("January", "31"), FEBRUARY("February", "28"), MARCH("March", "31"), APRIL("April", "30"),
+        MAY("May", "31"),
+        JUNE("June", "30"),
+        JULY("July", "31"), AUGUST("August", "31"), SEPTEMBER("September", "30"), OCTOBER("October", "31"),
+        NOVEMBER("November", "30"),
+        DECEMBER("December", "31");
+
+        private final String monthName;
+        private final String monthDays;
+
+        private Month(final String monthName, final String monthDays) {
+            this.monthName = monthName;
+            this.monthDays = monthDays;
+        }
+
+        public String getName() {
+            return this.monthName;
+        }
+
+        public String getMonthDays() {
+            return this.monthDays;
+        }
+
+        public static Month fromString(final String name) {
+         for(final Month m : Month.values()) {
+            if(m.getName().equals(name)) {
+                return m;
+            } 
+          }
+          throw new IllegalArgumentException("Unknown Month name: " + name);
+        }
 
     @Override
     public Comparator<String> sortByDays() {
